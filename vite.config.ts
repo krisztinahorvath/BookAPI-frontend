e98d1bac232da6daa0a748c9a03a9d79c8b5b0f8
Krisztina Horvath <91715582+krisztinahorvath@users.noreply.github.com>
2023-05-15 18:32:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
  plugins: [react()],
- define: {
- global: {},},
+ //define: { // needed when working on localhost
+ //global: {},},
 
 });
